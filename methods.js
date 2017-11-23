c40eed6cdb9e3fec09d92d2d626b205fecadae12
Arun Biswas <arunbsmrstu@gmail.com>
2017-11-23 14:25:21 +0600

    Node js

---
+++ methods.js
@@ -0,0 +1,16 @@
+var methods= { };
+
+methods.updateServer=function(){
+
+    console.log('Important Function');
+}
+
+methods.cockies =function(){
+    console.log('In cockies function');
+}
+
+methods.node=function(){
+    console.log('Node JS,is awosam !');
+}
+
+exports.data=methods;
